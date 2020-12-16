8e89c1059d88a5f7a183e2526f929550ce2c7b17
gsyoung2020 <73722590+gsyoung2020@users.noreply.github.com>
2020-12-16 18:05:49 -0500

    -version 2

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
+//maybe place the game play loop in one thing
+// change all X and Y letters swap them !!!!!!!
 const battleship = () => {
   alert(`Your about to playing 4x4 battleship! Your ships will be randomly placed, the prompts given and remeber this game is ZERO-based that means the board actually starts at 0x0, which means hitting things in the first postition needs to be at zero `)
-  
+
   let player1 = {
     name: prompt (`Player 1 Submit To Me Your NAME`),
-    player1Lives: 3, //supposed to be 4??
+    player1Lives: 4, //supposed to be 4??
     gameBoard: [[0,0,0,0],
-               [0,0,0,0],
-               [0,0,0,0],
-               [0,0,0,0]],        
+                [0,0,0,0],
+                [0,0,0,0],
+                [0,0,0,0]],        
     p1Guess: []
   }
   getRandomNumber = (max) => {
@@ -16,15 +18,17 @@ const battleship = () => {
   for( let i=0; i<player1.player1Lives; i++){
       let randomX = getRandomNumber(4);
       let randomY = getRandomNumber(4);
-      player1.gameBoard[randomX][randomY] = 1;
-//if (randomX == gameBoard[randomX][i] && randomY == gameBoard[i][randomX]){}
-  console.log(`X: ${randomX} Y: ${randomY}`)
-   //console.log(player1.gameBoard[randomX][randomY])
-   //console.log(player1.assignShips)
+      if (player1.gameBoard[randomY][randomX] == 0){
+         player1.gameBoard[randomY][randomX] =1;        
+      }
+      //if (randomX == gameBoard[randomX][i] && randomY == gameBoard[i][randomX]){}
+      console.log(`X: ${randomX} Y: ${randomY}`)
+      //console.log(player1.gameBoard[randomX][randomY])
+      //console.log(player1.assignShips)
   }
   console.log(player1.gameBoard) 
-    
-    let player2 = {
+
+  let player2 = {
     name: prompt (`Player 2 Submit To Me Your Name`),
     player2Lives: 4,
     gameBoard: [[0,0,0,0],
@@ -32,43 +36,62 @@ const battleship = () => {
                [0,0,0,0],
                [0,0,0,0]],       
     p2Guess: []
-}
+  }
   getRandomNumber = (max) => {
     return Math.floor(Math.random() * Math.floor(max));
-}
+  }
   for( let i=0; i<player2.player2Lives; i++){
       let randomX = getRandomNumber(4);
       let randomY = getRandomNumber(4);
-      player2.gameBoard[randomX][randomY] = 1;
-//if (randomX == gameBoard[randomX][i] && randomY == gameBoard[i][randomX]){}
+//look into while loops
+  if (player2.gameBoard[randomY][randomX] == 0){
+     player2.gameBoard[randomY][randomX] = 1;
+  };
   console.log(`X: ${randomX} Y: ${randomY}`)
-   //console.log(player1.gameBoard[randomX][randomY])
+   //console.log(player2.gameBoard[randomY][randomX])
    //console.log(player1.assignShips)
-}
+  }
   console.log(player2.gameBoard) 
 
-p1game()
-  
-  
-function p1game () {
-  alert(`${player1.name} 's Turn`)
-  var posX = parseInt(prompt(`give me X`))
-  let posY = parseInt(prompt(`give me Y`))
-console.log(player2.gameBoard[posX][posY])
+  p1game()
+
+  /*
+  function lifeCheck () {
+    if (player1.player1Lives == 0) {
+      alert(`HEY ${player2.name} YOUR A WINNER !!!!`) 
+    }
+    if (player2.player2Lives == 0 ) {
+      alert(`HEY ${player1.name} YOUR A WINNER !!!!`) 
+    }
+  }
+  */
+
+  function p1game () {
+    alert(`${player1.name} 's Turn insert a number between 0-3`)
+    var posX = parseInt(prompt(`give me X`))
+    let posY = parseInt(prompt(`give me Y`))
+console.log(player2.gameBoard[posY][posX])
+ /*
  if (player2.player2Lives == 0 ) {
    alert(`HEY ${player1.name} YOUR A WINNER !!!!`)
    player2.gameBoard[posX][posY] = 0
  }
-else if (player2.gameBoard[posX][posY] == 1){
-  alert(`${player1.name} YOU GOTTA HIT`)
-  player1.p1Guess.push(`Pos-X ${posX}, Pos-Y ${posY}`)
-  alert(`$ Your Guesses are/were ${player1.p1Guess}`)
-  player2.player2Lives--;
-  player2.gameBoard[posX][posY] = 0
-  console.log(`these are ${player2.name}'s  ${player2.player2Lives} lives`)
-  p2game()
-  } 
-  else if (player2.gameBoard[posX][posY] !== 1) {
+ */
+if (player2.gameBoard[posY][posX] == 1){
+   alert(`${player1.name} YOU GOTTA HIT`)
+   player1.p1Guess.push(`Pos-X ${posX}, Pos-Y ${posY}`)
+   alert(`$ Your Guesses are/were ${player1.p1Guess}`)
+   player2.player2Lives--;
+   player2.gameBoard[posY][posX] = 0
+   console.log(`these are ${player2.name}'s  ${player2.player2Lives} lives`)
+if (player1.player1Lives == 0) {
+   alert(`HEY ${player2.name} YOUR A WINNER !!!!`) 
+ } else if (player2.player2Lives == 0   ) {
+   alert(`HEY ${player1.name} YOUR A WINNER !!!!`) 
+} else {
+    p2game()
+    }
+  } else if (player2.gameBoard[posY][posX] !== 1) {
   alert(`${player1.name} sorry but you didn't hit...`)
   player1.p1Guess.push(`Pos-X ${posX}, Pos-Y ${posY}`)
   alert(`Your Guesses are/were ${player1.p1Guess}`)
@@ -77,34 +100,45 @@ else if (player2.gameBoard[posX][posY] == 1){
 }
 
 function p2game () {
-  alert(`${player2.name} 's Turn`)
+  alert(`${player2.name} 's Turn insert a number between 0-3` )
   var posX = parseInt(prompt(`give me X`))
   let posY = parseInt(prompt(`give me Y`))
-console.log(player1.gameBoard[posX][posY])
+console.log(player1.gameBoard[posY][posX])
+ /*
  if (player1.player1Lives ==0) {
    alert(`HEY ${player2.name} YOUR A WINNER !!!!`)
-   player1.gameBoard[posX][posY] = 0
+   player1.gameBoard[posY][posX] = 0
  }
-else if (player1.gameBoard[posX][posY] == 1){
+ */
+if (player1.gameBoard[posY][posX] == 1){
   alert(`${player2.name} YOU GOTTA HIT`)
   player2.p2Guess.push(`Pos-X ${posX}, Pos-Y ${posY}`)
   alert(`Your Guesses are/were ${player2.p2Guess}`)
   player1.player1Lives--;
-  player1.gameBoard[posX][posY] = 0
+  player1.gameBoard[posY][posX] = 0
   console.log(`these are ${player1.name}'s  ${player1.player1Lives} lives`)
-  p1game()
-  } 
-  else if (player1.gameBoard[posX][posY] !== 1) {
+  if (player1.player1Lives == 0) {
+   alert(`HEY ${player2.name} YOUR A WINNER !!!!`) 
+ }
+  else if (player2.player2Lives == 0   ) {
+   alert(`HEY ${player1.name} YOUR A WINNER !!!!`) 
+} else {
+    p1game()
+    }
+  } else if (player1.gameBoard[posY][posX] !== 1) {
   alert(`${player2.name} sorry but you didn't hit...`)
   player2.p2Guess.push(`Pos-X ${posX}, Pos-Y ${posY}`)
   alert(`$ Your Guesses are/were ${player2.p2Guess}`)
   p1game()
   }
 
+
 }
-  console.log('The winner is...?')
+console.log('The winner is...?')
 }
+
 const gameResult = battleship()
+
 
 
 const htmlTarget = document.getElementById('result')
